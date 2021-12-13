eee3fad1c63cfd0bd6587b2b182cc6b46ef84c8f
Lloyd W Turk Jr <xerodill@gmail.com>
2021-12-13 15:59:05 -0500

    Finished, basic CRUD App

--- app.py
+++ app.py
@@ -43,6 +43,22 @@ def delete(id):
     except:
         return "There was a problem deleting that task"
 
+@app.route("/update/<int:id>", methods=["GET", "POST"])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == "POST":
+        task.content = request.form["content"]
+
+        try: 
+            db.session.commit()
+            return redirect("/")
+        
+        except:
+            return "Failed to update Task."
+    else:
+        return render_template("update.html", task=task)
+
 if __name__ == "__main__":
     #app.run(debug=True)
     from waitress import serve
