81c76a11d1eb5c5d29cc89b9becc26504d6b7d38
Lloyd W Turk Jr <xerodill@gmail.com>
2021-12-17 10:07:52 -0500

    HTML

--- PythonOnTheWebLearningFlask/app/views.py
+++ PythonOnTheWebLearningFlask/app/views.py
@@ -1,11 +1,11 @@
 from app import app
 
-from flask import render_template, url_for
+from flask import render_template
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("public/index.html")
 
 @app.route("/about")
 def about():
