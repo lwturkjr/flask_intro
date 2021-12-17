12569c153f24dfc9c0a550a3d60d4a4c7c45f552
lwturkjr <lwturkjr@SgtSegFault.home>
2021-12-16 20:36:44 -0500

    html

--- PythonOnTheWebLearningFlask/app/views.py
+++ PythonOnTheWebLearningFlask/app/views.py
@@ -1,8 +1,11 @@
 from app import app
 
+from flask import render_template, url_for
+
+
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
